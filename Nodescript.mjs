3df4cb858dc2297789e746c72131d46fd9afca6c
Abhinav B <behaniyaabhinav@gmail.com>
2024-02-17 10:19:10 +0530

    Update Nodescript.mjs

--- Nodescript.mjs
+++ Nodescript.mjs
@@ -3,40 +3,12 @@ import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 const app = express();
-// const PORT = 3000;
+
 
 const PORT = process.env.PORT || 3000; // Use 3000 or another default port if running locally
 
 
-// const app = express();
-// const PORT = 3000;
 
-// // Enable CORS for all routes
-// app.use(cors());
-
-// // Define the API URL
-// const apiUrl = 'https://alfa-leetcode-api.vercel.app';
-
-// // Define a route to fetch data from the API
-// app.get('/fetchData', async (req, res) => {
-//   try {
-//     const response = await fetch(`${apiUrl}/daily`);
-//     const data = await response.json();
-
-//     // Send the JSON data to the client
-//     res.json(data);
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running at http://localhost:${PORT}`);
-// });
-// import express from 'express';
-// import cors from 'cors';
 
 let globalUsername = '';
 // const app = express();
@@ -52,18 +24,7 @@ app.post('/setusername', (req, res) => {
   res.json({ message: 'Username received successfully' });
 });
 
-// // Other endpoints and routes...
 
-// app.listen(3000, () => {
-//   console.log('Server running on port 3000');
-//   console.log(globalUsername);
-// });
-
-// // Activate this server using node Nodescript.mjs in cmd.
-// import express from 'express';
-// 
-// import cors from 'cors';
-// import fetch from 'node-fetch';
 
 
 
@@ -76,7 +37,7 @@ const apiUrl = 'https://alfa-leetcode-api.vercel.app';
 // Define routes for different endpoints
 app.get('/getusername', async (req, res) => {
   try {                          
-    // const response = await fetch('https://alfa-leetcode-api.vercel.app/AbhinavB1203')
+   
     const response = await fetch(`${apiUrl}/${globalUsername}`);
     const data = await response.json();
     res.json(data);
